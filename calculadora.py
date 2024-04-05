bd70768145a88e32b6da2463029fe11c742ed6b4
Marcos Gregorios <61382919+Gregoriodata@users.noreply.github.com>
2024-04-05 19:52:22 -0300

    Includ While

--- calculadora.py
+++ calculadora.py
@@ -1,32 +1,27 @@
 import math
-dict3 = {}
-8 + 'x'
 
 print('***********Calculadora em Python***********')
-
-nome = input("Qual seu nome?")
-print(f'Seja bem vindo, {nome}')
-print('Selecione o número da operação desejada:\n' +
-      '1 - Soma\n' +
-      '2 - Subtração\n' +
-      '3 - Multiplicação\n' +
-      '4 - Divisão')
-n2 = int(input(f'Então, vamos realizar qual tipo de operação? (1, 2, 3 ou 4):'))
-n3 = [n2]
-if n2 == 1:
-       for i in n3:
-        print("Ok, você digitou o nº1 -")
-        num1 = int(input('Informe o primeiro número: '))
-        num2 = int(input('Informe o segundo número: '))
-        tot_1 = num1 + num2
-        print(f'Você informou os números, {num1} e {
-            num2}, e a soma dos valore é {tot_1}.')
-        n4 = int(input('Deseja fazer mais alguma operação?, se sim - 1, se não 2'))
-                    
-        numvale = int(input('Digite o numero por favor: '))
-
-else:
-    print('Muito obrigado, volte sempre')
+n4 = 1  
+while n4 == 1:
+    nome = input("Qual seu nome?")
+    print(f'Seja bem vindo, {nome}')
+    print('Selecione o número da operação desejada:\n' +
+        '1 - Soma\n' +
+        '2 - Subtração\n' +
+        '3 - Multiplicação\n' +
+        '4 - Divisão')
+    n2 = int(input(f'Então, vamos realizar qual tipo de operação? (1, 2, 3 ou 4):'))
+    n3 = [n2]
+  
+    if n2 == 1:
+            print("Ok, você digitou o nº1 -")
+            num1 = int(input('Informe o primeiro número: '))
+            num2 = int(input('Informe o segundo número: '))
+            tot_1 = num1 + num2
+            print(f'Você informou os números, {num1} e {
+                num2}, e a soma dos valore é {tot_1}.')
+            n4 = int(input('Deseja fazer mais alguma operação?, se sim - 1, se não 2'))
+print('Muito obrigado, volte sempre')
 
 
 
@@ -53,5 +48,5 @@ else:
     #     tot_2 = num1 * num2
     #     print(f'Você informou os números, {num1} e {
     #           num2}, e a Subtração dos valore é {tot_2}.')
-else:
-    print("Ixi, infelizmente você digitou um numero inválido, tente novamente")
+# else:
+#     print("Ixi, infelizmente você digitou um numero inválido, tente novamente")
