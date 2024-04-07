e258ab856f337a0a1fc3f0531f00b159b44546c8
Marcos Gregorios <61382919+Gregoriodata@users.noreply.github.com>
2024-04-07 18:08:57 -0300

    Desenvolvimento dos módulos de multiplicação, divisão e subtração / ajuste na regra do while

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,9 @@
+"""
+Objetivo do cod: Programa desenvolvido para resolver contas matematicas.
+Autor: Marcos Gregório
+"""
+
+
 import math
 import time
 
@@ -14,11 +20,11 @@ while n4 == 1:
     n2 = int(input(f'Então, vamos realizar qual tipo de operação? (1, 2, 3 ou 4):'))
     # n3 = [n2]
     if n2 == 1:
-        print("Ok, você digitou o nº1 -")
+        print("Ok, você digitou o nº1, então vamos iniciar a soma")
         Continuar = 's'
         while Continuar.lower() == 's':
-            num1 = int(input('Informe o primeiro número: '))
-            num2 = int(input('Informe o segundo número: '))
+            num1 = int(input('Primeiro informe um número: '))
+            num2 = int(input('Agora, informe o segundo número: '))
             tot_1 = num1 + num2
             print('Calculando...')
             time.sleep(2)
@@ -29,31 +35,59 @@ while n4 == 1:
             print(f'Você informou os números, {num1} e {
                   num2}, e a soma dos valore é {tot_1}.')
             Continuar = str(
-                input('Deseja fazer mais alguma operação de soma? Se sim digite s, se não n."'))
-    n4 = int(input('Deseja fazer mais alguma operação?, Se sim - 1, Se não 2: '))
+                input('Deseja fazer mais alguma operação de soma? Se sim digite S, se não N."'))
+    elif n2 == 2:
+        print("Ok, você digitou o nº2, então vamos iniciar a subtração")
+        Continuar2 = 's'
+        while Continuar2.lower() == 's':
+            num1 = int(input('Primeiro informe um número: '))
+            num2 = int(input('Agora, informe o segundo número: '))
+            tot_2 = num1 - num2
+            print('Calculando...')
+            time.sleep(2)
+            print('Só mais um momento...')
+            time.sleep(2)
+            print('Agrupando os dados...')
+            time.sleep(2)
+            print(f'Você informou os números, {num1} e {
+                num2}, e a multiplicação dos valore é {tot_2}.')
+            Continuar2 = str(
+                input('Deseja fazer mais alguma operação de subtração? Se sim digite S, se não N."'))
+    elif n2 == 3:
+        print("Ok, você digitou o nº3, então vamos iniciar a multiplicação")
+        Continuar3 = 's'
+        while Continuar3 == 's':
+            num1 = int(input('Informe o primeiro número: '))
+            num2 = int(input('Informe o segundo número: '))
+            tot_2 = num1 * num2
+            print('Calculando...')
+            time.sleep(2)
+            print('Só mais um momento...')
+            time.sleep(2)
+            print('Agrupando os dados...')
+            time.sleep(2)
+            print(f'Você informou os números, {num1} e {
+                num2}, e a Subtração dos valore é {tot_2}.')
+            Continuar3 = str(
+                input('Deseja fazer mais alguma operação de multiplicação? Se sim digite S, se não N."'))
+    elif n2 == 4:
+        print("Ok, você digitou o nº4, então vamos iniciar a divisão")
+        Continuar4 = 's'
+        while Continuar4 == 's':
+            num1 = int(input('Informe o primeiro número: '))
+            num2 = int(input('Informe o segundo número: '))
+            tot_2 = num1 / num2
+            print('Calculando...')
+            time.sleep(2)
+            print('Só mais um momento...')
+            time.sleep(2)
+            print('Agrupando os dados...')
+            time.sleep(2)
+            print(f'Você informou os números, {num1} e {
+                  num2}, e a divisão dos valore é {tot_2}.')
+            Continuar4 = str(
+                input('Deseja fazer mais alguma operação de multiplicação? Se sim digite S, se não N."'))
+    else:
+        print("Ixi, infelizmente você digitou um numero inválido, tente novamente")
+        n4 = int(input('Deseja fazer mais alguma operação?, Se sim - 1, Se não 2: '))
 print('Muito obrigado, volte sempre')
-
-# elif n2 == 2:
-#     print("Ok, você digitou o nº2 -")
-#     num1 = int(input('Informe o primeiro número: '))
-#     num2 = int(input('Informe o segundo número: '))
-#     tot_2 = num1 - num2
-#     print(f'Você informou os números, {num1} e {
-#           num2}, e a Subtração dos valore é {tot_2}.')
-# elif n2 == 3:
-#     print("3 - Multiplicação")
-#     num1 = int(input('Informe o primeiro número: '))
-#     num2 = int(input('Informe o segundo número: '))
-#     tot_2 = num1 * num2
-#     print(f'Você informou os números, {num1} e {
-#           num2}, e a Subtração dos valore é {tot_2}.')
-
-# elif n2 == 4:
-#     print("4 - Divissao")
-#     num1 = int(input('Informe o primeiro número: '))
-#     num2 = int(input('Informe o segundo número: '))
-#     tot_2 = num1 * num2
-#     print(f'Você informou os números, {num1} e {
-#           num2}, e a Subtração dos valore é {tot_2}.')
-# else:
-#     print("Ixi, infelizmente você digitou um numero inválido, tente novamente")
